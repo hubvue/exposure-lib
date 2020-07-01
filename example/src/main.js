644c00a200766e58559d483c87cd69f6b2562adc
didi <1191340528@qq.com>
2020-07-01 15:21:24 +0800

    chore: example

--- example/src/main.js
+++ example/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Exposure from '../../dist/exposure'
+// import Exposure from '../../dist/exposure'
+import Exposure from 'vue-exposure/dist/exposure-polyfill'
 import router from './router/router'
 Vue.config.productionTip = false
 
