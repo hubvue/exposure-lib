ff6b8dc62c4bcd3e8bc6e9ddde5505f359fb82e2
hubvue <1191340528@qq.com>
2022-01-10 15:59:55 +0800

    chore: delete ci

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import Exposure from '@vue-exposure/polyfill'
-import Exposure from '../../src'
+import Exposure from '@vue-exposure/next'
 // ts-ignore
 createApp(App).use(store).use(router).use(Exposure).mount('#app')
